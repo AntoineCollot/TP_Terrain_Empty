87252f0f6aeae97ec454f78906fb74c66ea73d42
AntoineCollot <antoine.collot@live.com>
2021-03-29 00:07:19 +0200

    Initial commit

---
+++ Assets/Scripts/GenerateMeshFromHeightMap.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMeshFromHeightMap : MonoBehaviour
+{
+    public Texture2D heightMap = null;
+    Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        Generate();
+    }
+
+    public void GetRandomMap()
+    {
+        
+    }
+
+    public void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = GenerateMesh();
+    }
+
+    Mesh GenerateMesh()
+    {
+        if (heightMap == null)
+            GetRandomMap();
+
+        if (heightMap.width > 256 || heightMap.height > 256)
+            Debug.LogWarning("HeightMap is too big, it may cause issues");
+
+        Vector3[] points = GeneratePointsFromHeightMap(heightMap.width, heightMap.height, heightMap);
+        int[] triangles = GenerateTriangles(heightMap.width, heightMap.height);
+        Vector2[] uvs = GenerateUVs(heightMap.width, heightMap.height);
+
+        mesh.vertices = points;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+
+    Vector3[] GeneratePointsFromHeightMap(int width, int height, Texture2D heightMap)
+    {
+        return null;
+    }
+
+    int[] GenerateTriangles(int width, int height)
+    {
+        return null;
+    }
+
+    Vector2[] GenerateUVs(int width, int height)
+    {
+        //UVs are in 2D
+        Vector2[] uvs = new Vector2[width * height];
+
+        //Map the UVs left to right, bottom to top
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                //Get the id in the 1D array.
+                //The ids are from left to right, bottom to top.
+                int id = y * width + x;
+
+                uvs[id] = new Vector2(x / (float)width, y / (float)height);
+            }
+        }
+
+        return uvs;
+    }
+}
---
+++ Assets/Scripts/MorphTerrain.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorphTerrain : MonoBehaviour
+{
+    public KeyCode startMorphKey = KeyCode.Space;
+    public float animTime = 2;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(startMorphKey))
+        {
+            //Stop any coroutine on going to avoid creating a mess if spamming the button
+            StopAllCoroutines();
+            //Start the anim
+            StartCoroutine(Morph());
+        }
+    }
+
+    IEnumerator Morph()
+    {
+        float t = 0;
+
+        //Scale down loop
+        while(t<1)
+        {
+            t += Time.deltaTime / animTime * 2;
+
+            yield return null;
+        }
+
+        //Reset the progress variable
+        t = 0;
+
+        //Scale Up loop
+        while (t < 1)
+        {
+            t += Time.deltaTime / animTime * 2;
+
+            yield return null;
+        }
+    }
+}
